fcde9d4808cf60c0a008a724f8c2b952e2c08acd
mgrzadziel <mgrzadziel@outlook.com>
2019-06-13 21:06:35 +0200

    My first Commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;  // framework Identity
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signInMgr) //konstruktor inicjaluzujący 
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+        }
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl) // Udostępnia widok logowania Model - Kontoler zarządza LoginModelem Odwołanie do Widoku
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken] // do logowania 
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Nieprawidłowa nazwa użytkownika lub hasło");
+            return View(loginModel);
+            
+        }
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using Microsoft.AspNetCore.Authorization;
+namespace SportsStore.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+
+        public ViewResult Edit(int productId) =>
+            View(repository.Products
+                .FirstOrDefault(p => p.ProductID == productId));
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"Zapisano {product.Name}.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Błąd w wartościach danych.
+                return View(product);
+            }
+            
+        }
+        public ViewResult Create() => View("Edit", new Product());
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = $"Usunięto {deletedProduct.Name}.";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
